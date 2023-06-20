77ce954204efd04626e9776a3590d5be1508323f
sol0916 <yj0916lee00@gmail.com>
2023-06-20 21:19:46 +0900

    20230620 commit

--- src/programers/ProTest.java
+++ src/programers/ProTest.java
@@ -1,11 +1,27 @@
 package programers;
 
-import java.util.Arrays;
-
 public class ProTest {
-
 	public static void main(String[] args) {
-
-	
+		
+		int[] num_list = {4, 2, 6, 1, 7, 6};
+		
+		int even = 0;
+		int odd = 0;
+		
+		for(int i=0; i<num_list.length; i++) {
+			
+			if(i%2==0) {
+				odd += num_list[i];
+			} else {
+				even += num_list[i];
+			}						
+		}
+		
+		int answer = odd>even ? odd : even;
+		
+		System.out.println(answer);
+		
+		
 	}
+
 }
---
+++ src/study/BJ1260.java
@@ -0,0 +1,39 @@
+package study;
+
+import java.io.*;
+import java.util.*;
+
+public class BJ1260 {
+
+	public static void main(String[] args) throws IOException {
+
+
+		/* 
+		 * DFS = 깊이 우선 탐색 (stack)
+		 * BFS = 넓이 우선 탐색 (queue)
+		 */
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		br.readLine();
+		
+		String str = "";
+		
+		Stack stack = new Stack();
+		
+		
+		
+		
+		
+		
+		
+		bw.write(str);
+		
+		bw.flush();
+		bw.close();
+		
+		
+
+	}
+}
