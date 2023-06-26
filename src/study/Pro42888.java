7a0a0f73b3323d33b091fd5b7b6b1b3966c337b1
sol0916 <yj0916lee00@gmail.com>
2023-06-26 20:51:26 +0900

    20230626 commit

--- src/study/Pro42888.java
+++ src/study/Pro42888.java
@@ -5,23 +5,37 @@ import java.util.*;
 public class Pro42888 {
 
 	public static void main(String[] args) {
-		
-		//오픈채팅방 - 정규표현식 문제
-		
-		String[] recode = {"Enter uid1234 Muzi", 
-				"Enter uid4567 Prodo",
-				"Leave uid1234",
-				"Enter uid1234 Prodo",
-				"Change uid4567 Ryan"}; 
 
-		
+		//오픈채팅방
+
+		String[] record = {"Enter uid1234 Muzi", 
+						   "Enter uid4567 Prodo",
+						   "Leave uid1234",
+						   "Enter uid1234 Prodo",
+						   "Change uid4567 Ryan"}; 
+
+		String[] answer = new String[record.length];
 		Map<String, String> map = new HashMap<String, String>();
-		
-		for(int i=0; i<recode.length; i++) {
-			String[] arr = recode[i].split(" ");
-			map.put(arr[1], arr[2]);
-			
+
+		for(int i=0; i<record.length; i++) {
+			String[] arr = record[i].split(" ");
+						
+			switch(arr[0]) {
+			case "Enter":
+				map.put(arr[1], arr[2]);			
+				answer[i] = map.get(arr[1])+"님이 들어왔습니다.";
+				break;
+			case "Leave":
+				answer[i] = map.get(arr[1])+"님이 나갔습니다.";
+				map.remove(arr[1]);
+				break;
+			case "Change":
+				map.put(arr[1], arr[2]);
+				break;
+			}
 		}
+		
+		System.out.println(Arrays.toString(answer));
 	}
 
 }
