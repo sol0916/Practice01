7b1f1ac7315ee34b4b1b8fad7b8a71fcfbad0afe
sol0916 <yj0916lee00@gmail.com>
2023-07-07 17:12:34 +0900

    20230707 commit

--- src/study/BJ17140.java
+++ src/study/BJ17140.java
@@ -1,9 +1,17 @@
 package study;
 
+import java.util.*;
+
 public class BJ17140 {
 
 	public static void main(String[] args) {
+		
+		//이차원 배열과 연산
+		
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
 
+		
+		
 	}
 
 }
